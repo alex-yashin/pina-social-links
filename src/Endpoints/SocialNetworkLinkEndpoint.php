d57c9ef3acf4ceee16149a67b0d0c1dabdd850b5
Alex Yashin <alex-yashin@ya.ru>
2023-07-02 18:52:17 +0400

    переводим заголовки на русский

--- src/Endpoints/SocialNetworkLinkEndpoint.php
+++ src/Endpoints/SocialNetworkLinkEndpoint.php
@@ -7,13 +7,15 @@ use Pina\App;
 use Pina\Http\DelegatedCollectionEndpoint;
 use Pina\Http\Request;
 
+use function Pina\__;
+
 class SocialNetworkLinkEndpoint extends DelegatedCollectionEndpoint
 {
 
     public function __construct(Request $request)
     {
         parent::__construct($request);
-        $this->composer->configure('Social network links', "Create social network link");
+        $this->composer->configure(__('Ссылки на социальные сети'), __('Добавить ссылку на социальную сеть'));
         $this->collection = App::make(SocialNetworkLinkCollection::class);
     }
 
